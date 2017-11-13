c168019a8c2b43c321d9062dbc9b18b1223ae0a4
tatt61880 <tatt61880@gmail.com>
2017-11-13 22:24:06 +0900

    .

---
+++ _/main.rb
@@ -0,0 +1,35 @@
+require 'find'
+require "open3"
+
+curDir = Dir.pwd
+Dir::chdir("..")
+dir = Dir.pwd
+
+count = 0
+Find.find(dir) {|fpath|
+	Find.prune if(fpath == curDir)
+	if fpath =~ /main.kn$/
+		out, err, status = Open3.capture3("cmd.exe /Q /C \"kuincl -i #{fpath} -e cui -q > #{curDir}/output.txt\"")
+		File.open(curDir + "/output.txt", 'r'){|f|
+			buff = f.read().encode("UTF-8", "UTF-16LE")
+			if buff =~ /^\[Error\]/
+				err = buff[8, 6]
+				if fpath =~ /#{err}\/\w+.kn$/
+					puts "#{err} ok"
+				else
+					puts "Error #{fpath} [#{err}]"
+					count += 1
+				end
+			else
+				puts "Error #{fpath}"
+				puts " #{buff}"
+				count += 1
+			end
+		}
+	end
+}
+if count == 0
+	puts "Congratulations!"
+else
+	puts "Count for unexpected result = #{count}."
+end
